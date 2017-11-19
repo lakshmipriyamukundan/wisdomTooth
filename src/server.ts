99c1ec9dbc347b5b6a3d93c88b67cf9b9174188e
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2017-11-19 23:05:41 +0530

    Initial commit'

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+
+//import routes or controllers
+
+class Server {
+
+    public app: express.Application;
+
+    constructor() {
+        
+    }
+}
