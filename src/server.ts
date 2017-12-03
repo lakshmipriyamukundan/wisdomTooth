2a04156b33c38137756ab1a15ff5268a2fa00fab
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2017-12-03 12:52:50 +0530

    server set up done

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import * as logger from 'morgan';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
+import * as path from 'path';
 
 //import routes or controllers
 
@@ -8,6 +15,41 @@ class Server {
     public app: express.Application;
 
     constructor() {
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    public config(): void {
+
+        const MONGO_URL : string = 'mongodb://localhost/wisdom-tooth'; 
+        mongoose.connect(MONGO_URL);
+
+        // express middleware
+        this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.use(cookieParser());
+        this.app.use(logger('dev'));
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
+
+        // cors
+    this.app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        next();
+      });
+
+    }
+
+    public routes(): void {
+        const router: express.Router = express.Router();
         
+            this.app.use('/', router);
     }
 }
+
+export default new Server().app;
