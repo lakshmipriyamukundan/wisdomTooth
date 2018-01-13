00c0117722bb32005e38b80e9d7e03b28d7e747f
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-01-13 15:15:02 +0530

    gitignore modified

--- build/controllers/main-page.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MainClass {
-    static renderMain(req, res) {
-        res.render('index', { title: 'Views' });
-    }
-}
-exports.MainClass = MainClass;
-//# sourceMappingURL=main-page.js.map
